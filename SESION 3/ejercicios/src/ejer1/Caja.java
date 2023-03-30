6e41d2bd796f9e7a255d5f912fbfa18b4d124387
JuniorPalomino <junior.palomino@ucsm.edu.pe>
2023-03-29 19:38:34 -0500

    SubidaEjercicio03susu

---
+++ SESION 3/ejercicios/src/ejer1/Caja.java
@@ -0,0 +1,20 @@
+package ejer1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author H P
+ */
+public class Caja<T> {
+    
+    private T CualquierObjeto;
+    private String Color;
+    public Caja(){
+        super();
+    }
+    
+}
---
+++ SESION 3/ejercicios/src/ejer1/Cajoneria.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejer1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ * @author H P
+ */
+public class Cajoneria<Y extends Bolsa> {
+
+    private ArrayList<Caja> lista = new ArrayList<Caja>();
+    private int tope;
+
+    public Cajoneria(int Tope) {
+
+        this.tope = Tope;
+    }
+
+    public void add(Caja objeto) {
+        if (lista.size() <= tope) {
+            lista.add(objeto);
+        } else {
+            throw new RuntimeException("no caben mas");
+        }
+    }
+
+}
