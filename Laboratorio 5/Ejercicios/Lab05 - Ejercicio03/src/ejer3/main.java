41efb7bd343691cbbf6f5fc29898d1bba7db436b
carlosavila <carlos.avila@ucsm.edu.pe>
2023-04-16 19:21:26 -0500

    Terminado Ejerc 3

---
+++ Laboratorio 5/Ejercicios/Lab05 - Ejercicio03/src/ejer3/main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejer3;
+
+/**
+ *
+ * @author Asus
+ */
+public class main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        int[] ArregloEnteros = {3, 9, 1, 4, 6, 8, 2, 5, 7};
+
+        Sort.brickSort(ArregloEnteros);
+
+    }
+}
