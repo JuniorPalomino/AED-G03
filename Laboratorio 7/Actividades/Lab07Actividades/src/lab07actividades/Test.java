b715bc15999627a60d0f7e802e64af84938d2b8b
JLaurenteT <josue.laurente@ucsm.edu.pe>
2023-05-03 20:40:31 -0500

    actividad1

---
+++ Laboratorio 7/Actividades/Lab07Actividades/src/lab07actividades/Test.java
@@ -0,0 +1,20 @@
+
+package lab07actividades;
+
+import actividad1.ExceptionIsEmpty;
+import actividad1.StackArray;
+
+public class Test {
+    public static void main(String[] args) throws ExceptionIsEmpty {
+        StackArray pilas= new StackArray(4);
+        //System.out.println(pilas.pop());
+        pilas.push(4);
+        pilas.push(1);
+        pilas.push(7);
+        pilas.push(9);
+        System.out.println("Tope de la pila: "+pilas.top());
+        System.out.println("La pila esta llena?: "+pilas.isFull());
+        System.out.println(pilas);
+    }
+}
+
