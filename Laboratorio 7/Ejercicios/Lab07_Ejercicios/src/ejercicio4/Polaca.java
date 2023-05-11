944cd588b2acfb52643aef9dd7711afe1813a23f
JLaurenteT <josue.laurente@ucsm.edu.pe>
2023-05-10 20:41:28 -0500

    ejercicio4 v1

---
+++ Laboratorio 7/Ejercicios/Lab07_Ejercicios/src/ejercicio4/Polaca.java
@@ -0,0 +1,17 @@
+package ejercicio4;
+
+public class Polaca {
+    
+    public void readFile(String ruta)
+    {
+        
+    }
+    public String intoPos(String x)
+    {
+        return null;
+    }
+    public int resultExpresion(String x)
+    {
+        return 0;
+    }
+}
