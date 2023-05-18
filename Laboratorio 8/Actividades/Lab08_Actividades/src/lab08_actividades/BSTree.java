6654624667f7fa9eb84052ab3937caa57c35b516
JuniorPalomino <junior.palomino@ucsm.edu.pe>
2023-05-17 20:20:48 -0500

    SubidaActividadesActualizades

--- Laboratorio 8/Actividades/Lab08_Actividades/src/lab08_actividades/BSTree.java
+++ Laboratorio 8/Actividades/Lab08_Actividades/src/lab08_actividades/BSTree.java
@@ -7,49 +7,190 @@ package lab08_actividades;
 /**
  *
  * @author H P
+ * @param <E>
  */
 public class BSTree<E extends Comparable<E>> {
-    
-    class Node{
-    
+
+    class Node<E> {
+
         protected E data;
-        protected Node lefth,right;
+        protected Node left, right;
 
         public Node(E data) {
-            this(data,null,null);
+            this(data, null, null);
         }
 
-        public Node(E data, Node lefth, Node right) {
+        public Node(E data, Node left, Node right) {
             this.data = data;
-            this.lefth = lefth;
+            this.left = left;
             this.right = right;
-        }        
-        
-        
+        }
+
     }
     private Node root;
+
+    public BSTree() {
+        this.root = null;
+    }
+
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
+    public void insert(E x) throws ItemDuplicated {
+        this.root = insertRec(x, this.root);
+    }
+
+    private Node<E> insertRec(E x, Node<E> actual) throws ItemDuplicated {
+        Node<E> res = actual;
+
+        if (actual == null) {
+
+            res = new Node<E>(x);
+
+        } else {
+            int resC = actual.data.compareTo(x);
+            if (resC == 0) {
+                throw new ItemDuplicated(x + "esta duplicado");
+            }
+            if (resC < 0) {
+                res.right = insertRec(x, actual.right);
+            } else {
+                res.left = insertRec(x, actual.left);
+            }
+        }
+        return res;
+    }
+
+    public E search(E x) throws ItemDuplicated {
+
+        Node<E> res = searchRec(x, this.root);
+
+        if (res == null) {
+            throw new ItemDuplicated("El dato" + x + "no esta");
+        }
+        return res.data;
+
+    }
+
+    private Node<E> searchRec(E x, Node<E> n) {
+
+        if (n == null) {
+            return null;
+        } else {
+            int resC = n.data.compareTo(x);
+            if (resC < 0) {
+                return searchRec(x, n.right);
+            } else if (resC > 0) {
+                return searchRec(x, n.left);
+            } else {
+                return n;
+            }
+        }
+    }
+
+    public void remove(E x) throws ItemDuplicated {
+
+        root = removeRec(x, this.root);
+
+    }
+
+    public Node<E> removeRec(E x, Node<E> actual) throws ItemDuplicated {
+
+        Node<E> res=actual;
         
-        public BSTree(){this.root=null;}
-        public boolean isEmpty(){return this.root==null;}
-    
-    public void insert(E x) throws ItemDuplicated{
-    
+        if (actual == null) {
+
+            throw new ItemDuplicated("Elemento no encontrado");
+
+        }
+
+        int resC = actual.data.compareTo(x);
+
+        if (resC < 0) {
+
+            res.right = removeRec(x,actual.right);
+
+        } else if (resC > 0) {
+
+            res.left = removeRec(x,actual.left);
+
+        } else {
+
+            if (actual.left != null && actual.right != null) {
+
+                res.data = (E) minRecover(actual.right).data;
+
+                res.right = minRemove(actual.right);
+
+            } else {
+
+                res = (actual.left != null) ? res.left : res.right;
+
+            }
+
+        }
+
+        return res;
+
     }
-    
-    public E search(E x) throws ItemDuplicated{
+
+    private Node<E> minRemove(Node<E> actual) {
+
+        if (actual.left != null) {          //Buscamos el minmo
+
+            actual.left = minRemove(actual.left);
+
+        } else {                            //Eliminamos el minimo
+
+            actual = actual.right;
+
+        }
+
+        return actual;
+
+    }
+
+    public E minRemove() {
+
+        E min = minRecover();
+
+        this.root = minRemove(this.root);
+
+        return min;
+
+    }
+
+    public E minRecover(){ 
         
-        return x;
+        Node aux=minRecover(this.root);
+
+        return (E) aux.data; 
+
+    } 
+
+    public Node<E> minRecover(Node<E> actual){ 
+
+        if(actual.left == null){ 
+
+            return actual; 
+
+        }else{ 
+
+            return minRecover(actual.left); 
+
+        } 
+
+    } 
     
-    }
-    public void remove(E x) throws ItemDuplicated{
     
-    }
+
     public void inOrden() {
-    
+
     }
-    
+
     public void inOrden(Node actual) {
-    
+
     }
-    
+
 }
