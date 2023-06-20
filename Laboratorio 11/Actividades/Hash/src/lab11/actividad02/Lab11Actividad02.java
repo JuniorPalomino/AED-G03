1419109b2c3e6947b40b3e74c10239351bedafa6
JuniorPalomino <junior.palomino@ucsm.edu.pe>
2023-06-19 20:41:54 -0500

    SubidaDeActividad2Avance

---
+++ Laboratorio 11/Actividades/Hash/src/lab11/actividad02/Lab11Actividad02.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab11.actividad02;
+
+/**
+ *
+ * @author H P
+ */
+public class Lab11Actividad02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Laboratorio 11/Actividades/Hash/src/lab11/actividad02/Register.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab11.actividad02;
+
+/**
+ *
+ * @author H P
+ */
+public class Register<E> implements Comparable<Register<E>> {
+    
+    protected int key;
+    protected E value;
+
+    public Register(int key, E value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public int compareTo(Register<E> r) {
+        return this.key - r.key;
+    }
+    
+    public boolean equals(Object o){
+    
+        if (o instanceof Register){
+        
+            Register<E> r=(Register<E>)o;
+            return r.key==this.key;
+            
+        
+        }
+        
+        return false;
+    }
+
+    public int getKey() {
+        return this.key;
+    }
+    
+    public String toString(){
+        return this.key+":"+this.value.toString();
+    }
+    
+}
